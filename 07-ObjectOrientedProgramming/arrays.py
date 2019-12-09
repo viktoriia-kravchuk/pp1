97e190facdcbaa4b188a7d8370671c2d26c0c6d7
viktoriia-kravchuk <vikakravchuk2@gmail.com>
2019-12-09 14:39:21 +0100

    during class

--- 07-ObjectOrientedProgramming/arrays.py
+++ 07-ObjectOrientedProgramming/arrays.py
@@ -3,6 +3,6 @@ class arrays():
     def print_in_col(array):
         for c in array:
             print(c)
-            
+                 
 my_array = [4,1,8,7,2]
 arrays.print_in_col(my_array)
---
+++ 07-ObjectOrientedProgramming/zad_5.py
@@ -0,0 +1,12 @@
+class Utwor():
+    def __init__(self,author,title,album,year):
+        self.author=author
+        self.title=title
+        self.album=album
+        self.year=year
+    def __str__(self):
+         return ('Wykonawca: {:>20}\nUtwór: {:>19}\nAlbum: {:>25}\nRok: {:>15}\n'.format(self.author,self.title,self.album,self.year))
+song=Utwor('Dawid Podsiadło','Nie ma fal','Małomiasteczkowy','2018')
+print(song)                 
+
+
