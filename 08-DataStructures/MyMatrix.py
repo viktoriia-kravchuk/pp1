b08174e987b7b846d4731ff46fa2cfeb04dcf90a
viktoriia-kravchuk <vikakravchuk2@gmail.com>
2019-12-16 14:40:26 +0100

    during

--- 08-DataStructures/MyMatrix.py
+++ 08-DataStructures/MyMatrix.py
@@ -1,3 +1,4 @@
+from random import randrange
 class matrix():
 
     @staticmethod
@@ -12,11 +13,37 @@ class matrix():
             # add row to matrix
             matrix.append(row)
         return matrix
+    @staticmethod    
+    def create_2(x,y):
+        w,h=y,x
+        matrix=[[0 for x in range(w)] for y in range(h)]   
+        return matrix  
+    @staticmethod
+    def create_unit(x):
+       m=matrix.create(x,x) 
+       return m
+    @staticmethod
+    def fill_random(matrix,m,n):
+        l=len(matrix)
+        for i in range(l):
+            k=len(matrix[i])
+            for j in range (k):
+                value=randrange(m,n+1)
+                matrix[i][j]=value
+        return matrix        
+    @staticmethod
+    def transponse(m):
+        for row in m:
+            m2=[[m[j][i] for j in range(len(m))] for i in range(len(m[0]))] 
+        return m2    
 
     @staticmethod
     def print(matrix):
         for row in matrix:
             print(row)
 
-m = matrix.create(4,3)
+m = matrix.create_2(3,5)
+m = matrix.fill_random(m,1,9)
 matrix.print(m)
+m2=matrix.transponse(m)
+matrix.print(m2)
